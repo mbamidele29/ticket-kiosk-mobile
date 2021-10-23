23e63b603d071cbf04ece88237a7c62ee295cd96
Michael.Bamidele <michael.bamidele@arm.com.ng>
2021-10-23 10:41:08 +0100

    made minor fixes

--- lib/features/home/presentation/widgets/upcoming_events.dart
+++ lib/features/home/presentation/widgets/upcoming_events.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ticket_kiosk/core/entities/event.dart';
+import 'package:ticket_kiosk/core/entities/kiosk_message.dart';
+import 'package:ticket_kiosk/core/service_locator.dart';
 import 'package:ticket_kiosk/core/values/colors.dart';
 import 'package:ticket_kiosk/core/values/styles.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ticket_kiosk/core/widgets/show_message.dart';
+import 'package:ticket_kiosk/features/home/domain/services/event_service.dart';
 import 'package:ticket_kiosk/features/home/presentation/widgets/upcoming_event_item.dart';
 
 class UpcomingEvents extends StatefulWidget {
@@ -14,6 +18,12 @@ class UpcomingEvents extends StatefulWidget {
 
 class _UpcomingEventsState extends State<UpcomingEvents> {
   List<Event> events=[];
+
+  @override
+    void initState() {
+      initEvents();
+      super.initState();
+    }
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -30,5 +40,14 @@ class _UpcomingEventsState extends State<UpcomingEvents> {
         ],
       ),
     );
+  }
+
+  void initEvents() async {
+    KioskMessage message=await serviceLocator<EventService>()();
+    if(!message.status || message.data==null)showMessage(context, message.status, message.message);
+    else{
+      events.addAll(message.data);
+      setState(() {});
+    }
   }
 }
